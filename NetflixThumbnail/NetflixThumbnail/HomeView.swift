23509ff9256fdb6b8073288ec86ef851bdd94134
pan3800 <pan173434@gmail.com>
2024-07-04 15:55:59 +0900

    서버에서 받은 이미지 추가하기(Asynclamge)

--- NetflixThumbnail/NetflixThumbnail/HomeView.swift
+++ NetflixThumbnail/NetflixThumbnail/HomeView.swift
@@ -21,6 +21,41 @@ struct HomeView: View {
                     .frame(width: 50, height: 50)
                     .padding()
             }
+            
+            AsyncImage(url: URL(string: "https://ios-poster-json.s3.ap-northeast-2.amazonaws.com/posters/0BigImagePoster/bigPoster.png")) { image in
+                image
+                    .resizable()
+                    .cornerRadius(10)
+                    .aspectRatio(contentMode: .fit) //화면 비율 맞추기
+                    .frame(width:300, height: 525)
+            } placeholder: {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.gray)
+                    .frame(width: 300, height: 500)
+                    .opacity(0.4)
+                    .overlay(
+                        ProgressView()
+                            .tint(Color.white)
+                    )
+            }
+            .overlay(alignment: .bottom) {
+                HStack {
+                    Button(action: {
+                        print("재생버튼 클릭됨")
+                    }, label: {
+                        Image(systemName: "play.fill")
+                    })
+                    .buttonStyle(.bordered)
+                    
+                    Button(action: {
+                        print("info 버튼 클릭됨")
+                    }, label: {
+                        Image(systemName: "info.circle")
+                    })
+                    .buttonStyle(.bordered)
+                }
+                .padding()
+            }
         }
         .background(.black)
         .foregroundStyle(.white)
